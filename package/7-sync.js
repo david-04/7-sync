2d9f0bc2bbb9369dbe4b33540314a311499aa0c8
David Hofmann <65802756+david-04@users.noreply.github.com>
2023-08-18 16:48:14 +1000

    Update version number

--- package/7-sync.js
+++ package/7-sync.js
@@ -100,7 +100,7 @@ process.on("beforeExit", () => {
         Application.main();
     }
 });
-const APPLICATION_VERSION = "1.1.1";
+const APPLICATION_VERSION = "1.1.2";
 const COPYRIGHT_OWNER = "David Hofmann";
 const COPYRIGHT_YEARS = "2022-2023";
 class Context {
